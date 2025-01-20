9efb57b5434dba1b4fc8e0a456da5e8c72675d96
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-01-20 14:15:39 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,6 @@ const corsOptions = {
     credentials: true,
 };
 
-
-
 // Apply CORS middleware
 app.use(cors(corsOptions));
 
@@ -124,10 +122,10 @@ app.post("/submit", async (req, res) => {
 
 // Global error handler (optional)
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    console.error("Error Stack:", err.stack);
     res.status(500).json({
         message: 'Something broke!',
-        error: process.env.NODE_ENV === 'development' ? err.message : undefined,
+        error: process.env.NODE_ENV === 'development' ? err.stack : undefined,
     });
 });
 
